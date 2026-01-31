48193fa28e847cc3fcdd96991ca4502bd5b79d85
qanszer <maluendajohnsteven@gmail.com>
2026-01-31 23:34:06 +0800

    e

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ const reasons = [
     "I love how you believe in me to work all things out in the end", // 36
     "I love how you challenge me to be a better version of myself", // 37 - Jan 30
     "I love when you randomly decide to be flirty", // 38
-    "", // 39
+    "I love your curves bb, so hypnotizing", // 39
     // Add more reasons here - just keep adding lines like above!
     // Remember to put a comma after each reason except the last one
 ];
